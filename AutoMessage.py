60303edf61bc67b0644b7e5d44fa41e35eeca7c6
Amir Hossein <82036027+AmirHkrg@users.noreply.github.com>
2022-04-21 01:12:05 +0430

    Update AutoMessage.py

--- AutoMessage.py
+++ AutoMessage.py
@@ -5,9 +5,9 @@ import webbrowser as web
 import pyautogui as pg
 from colorama import Fore
 
-def sendwhatmsg_to_group(group_id: str, message: str, time_hour: int, time_min: int, wait_time: int = 10,
+def sendWhatMsg_to_group(group_id: str, message: str, time_hour: int, time_min: int,
                          tab_close: bool = False, close_time: int = 3 ) -> None:
-    
+
     if time_hour not in range(25) or time_min not in range(60):
         print("Invalid time format")
 
@@ -33,7 +33,7 @@ def sendwhatmsg_to_group(group_id: str, message: str, time_hour: int, time_min: 
 
     if left_time < wait_time:
         raise CallTimeException(
-            "Call time must be greater than wait_time as web.shad.ir takes some time to load")
+            "Call time must be greater than wait_time as web.whatsapp.com takes some time to load")
 
     date = "%s:%s:%s" % (current_time.tm_mday, current_time.tm_mon, current_time.tm_year)
     time_write = "%s:%s" % (timehr, time_min)
@@ -57,7 +57,7 @@ def sendwhatmsg_to_group(group_id: str, message: str, time_hour: int, time_min: 
     if tab_close:
         close_tab(wait_time=close_time)
 
-def sendshadmsg_to_group(group_id: str, message: str, time_hour: int, time_min: int, wait_time: int = 10,
+def sendShadMsg_to_group(group_id: str, message: str, time_hour: int, time_min: int, wait_time: int = 10,
                          tab_close: bool = False, close_time: int = 3 ) -> None:
 
     if time_hour not in range(25) or time_min not in range(60):
@@ -109,17 +109,16 @@ def sendshadmsg_to_group(group_id: str, message: str, time_hour: int, time_min: 
     if tab_close:
         close_tab(wait_time=close_time)
 
-def sendwhatmsg(phone_no: str, message: str, time_hour: int, time_min: int, wait_time: int = 10,
+def sendWhatMsg(phone_no: str, message: str, time_hour: int, time_min: int, wait_time: int = 10,
                 tab_close: bool = False, close_time: int = 3) -> None:
 
     global sleep_time
     if "+" not in phone_no:
         raise CountryCodeException("Country code missing from phone_no")
+    timehr = time_hour
 
     if time_hour not in range(25) or time_min not in range(60):
         raise Warning("Invalid Time Format")
-
-    timehr = time_hour
 
     if time_hour == 0:
         time_hour = 24
@@ -167,7 +166,64 @@ def sendwhatmsg(phone_no: str, message: str, time_hour: int, time_min: int, wait
     if tab_close:
         close_tab(wait_time=close_time)
 
-def Function():
+def sendInstaDirect(message: str, time_hour: int, time_min: int, repeat_num: int, wait_time: int = 10,
+                         tab_close: bool = False, close_time: int = 3) -> None:
+
+    if time_hour not in range(25) or time_min not in range(60):
+        print("Invalid time format")
+
+    timehr = time_hour
+
+    if time_hour == 0:
+        time_hour = 24
+    call_second = (time_hour * 3600) + (time_min * 60)
+
+    current_time = time.localtime()
+    current_hour = current_time.tm_hour
+    current_minute = current_time.tm_min
+    current_second = current_time.tm_sec
+
+    if current_hour == 0:
+        current_hour = 24
+
+    current_to_second = (current_hour * 3600) + (current_minute * 60) + current_second
+    left_time = call_second - current_to_second
+
+    if left_time <= 0:
+        left_time = 86400 + left_time
+
+    if left_time < wait_time:
+        raise CallTimeException(
+            "Call time must be greater than wait_time as instagram.com takes some time to load")
+
+    date = "%s:%s:%s" % (current_time.tm_mday, current_time.tm_mon, current_time.tm_year)
+    time_write = "%s:%s" % (timehr, time_min)
+    with open("InstagramDirect_history.txt", "a", encoding='utf-8') as file:
+        file.write("Date: %s\nTime: %s\nMessage: %s\nRepeat Number" %
+                   (date, time_write, message, repeat_num))
+        file.write("\n--------------------\n")
+    sleep_time = left_time - wait_time
+    print(f"\nIn {sleep_time} seconds instagram.com will open and after {wait_time} seconds message will be delivered")
+    time.sleep(sleep_time)
+    web.open('https://www.instagram.com/direct/inbox/')
+    time.sleep(10)
+    width, height = pg.size()
+    pg.click(width / 4, height - height / 1.5)
+    pc.copy(message)
+    time.sleep(10)
+    width, height = pg.size()
+    pg.click(width / 2, height - height / 8)
+    time.sleep(2)
+    for i in range(repeat_num):
+        pg.hotkey('ctrl', 'v')
+        time.sleep(2)
+        pg.press('enter')
+        time.sleep(2)
+    print("\n" + Fore.GREEN + "Message sent successfully!")
+    if tab_close:
+        close_tab(wait_time=close_time)
+
+def run():
     command_input = input(Fore.LIGHTGREEN_EX + "Enter the desired number to continue >> ")
 
     if(command_input == "1"):
@@ -195,7 +251,7 @@ def Function():
             exit()
 
 
-        sendwhatmsg(Group_ID,Message_Text,Time_hr,Time_Min)
+        sendWhatMsg(Group_ID,Message_Text,Time_hr,Time_Min)
 
     elif(command_input == "2"):
         os.system("cls")
@@ -220,7 +276,7 @@ def Function():
             print(Fore.WHITE)
             exit()
 
-        sendwhatmsg_to_group(Group_ID,Message_Text,Time_hr,Time_Min)
+        sendWhatMsg_to_group(Group_ID,Message_Text,Time_hr,Time_Min)
 
     elif(command_input == "3"):
         os.system("cls")
@@ -245,8 +301,31 @@ def Function():
             print(Fore.WHITE)
             exit()
 
-        sendshadmsg_to_group(Group_ID,Message_Text,Time_hr,Time_Min)
+        sendShadMsg_to_group(Group_ID,Message_Text,Time_hr,Time_Min)
 
+    elif(command_input == "4"):
+        os.system("cls")
+        Message_Text = input("Enter Your Message                : ")
+        if(Message_Text == ""):
+            print("Field is mandatory")
+            print(Fore.WHITE)
+            exit()
+        Time_hr = int(input("Enter The Hour                    : "))
+        if(Time_hr == ""):
+            print("Field is mandatory")
+            print(Fore.WHITE)
+            exit()
+        Time_Min = int(input("Enter The Minute                  : "))
+        if(Time_Min == ""):
+            print("Field is mandatory")
+            print(Fore.WHITE)
+            exit()
+        repeat_num = int(input("Enter the number of messages sent : "))
+        if(repeat_num == ""):
+            print("Field is mandatory")
+            print(Fore.WHITE)
+            exit()
+        sendInstaDirect(Message_Text, Time_hr, Time_Min, repeat_num)
     elif(command_input == "0"):
         os.system("cls")
         print(Fore.WHITE)
@@ -257,15 +336,16 @@ def Function():
         print(Fore.WHITE)
 
 os.system('cls')
-print(Fore.LIGHTBLUE_EX + "<<  To send an automatic message to a WhatsApp number ..... select 1  >>")
-print("<<  To send an automatic message to a WhatsApp group ...... select 2  >>")
-print("<<  To send an automatic message to a Shad number or group  select 3  >>")
-print("<<  To close the program .................................. select 0  >>")
+print(Fore.LIGHTBLUE_EX + "<<  To send an automatic message to a WhatsApp number .......... select 1  >>")
+print("<<  To send an automatic message to a WhatsApp group ........... select 2  >>")
+print("<<  To send an automatic message to a Shad number or group ..... select 3  >>")
+print("<<  To send an automatic message to a Instagram Direct ......... select 4  >>")
+print("<<  To close the program ....................................... select 0  >>")
 print("\n")
-print("             --*        Author   :   Amirh_Krg        *--")
-print("             --* GitHub : Https://Github.com/AmirHKrg *--")
+print("                  --*        Author   :   Amirh_Krg        *--")
+print("                  --* GitHub : Https://Github.com/AmirHKrg *--")
 print("\n")
 
-Function()
+run()
 
 print(Fore.WHITE)
